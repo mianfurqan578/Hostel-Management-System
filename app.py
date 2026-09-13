6d04f64a88d0f348492b24980860dba01d7fc019
mianfurqan578 <mianfurqan578@gmail.com>
2026-09-13 19:03:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def get_db_connection():
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Qwerty@123",
+       password=os.getenv("DB_PASSWORD")
         database="hostel_management"
     )
     return conn
